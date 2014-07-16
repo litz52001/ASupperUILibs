5a63bc934ee3765b49d721a03aa82f6fddece242
litz52001 <litz52001@163.com>
2014-07-16 15:42:56 +0800

    日志log

---
+++ src/com/litz/Tools/Log.java
@@ -0,0 +1,73 @@
+package com.litz.Tools;
+
+public final class Log {
+	
+	/**
+	 * 调试开关
+	 */
+	public static boolean mDebug = false;
+
+	public static void init(final boolean debug) {
+		mDebug = debug;
+	}
+	
+	public static void e(final String tag,final String msg){
+		if (mDebug) {
+			android.util.Log.e(tag, msg);
+		}
+	}
+	
+	public static void e(final String tag,final String msg,final Throwable tr){
+		if (mDebug) {
+			android.util.Log.e(tag, msg,tr);
+		}
+	}
+	
+	public static void w(final String tag,final String msg){
+		if (mDebug) {
+			android.util.Log.w(tag, msg);
+		}
+	}
+	
+	public static void w(final String tag,final String msg,final Throwable tr){
+		if (mDebug) {
+			android.util.Log.w(tag, msg,tr);
+		}
+	}
+	
+	public static void d(final String tag,final String msg){
+		if (mDebug) {
+			android.util.Log.d(tag, msg);
+		}
+	}
+	
+	public static void d(final String tag,final String msg,final Throwable tr){
+		if (mDebug) {
+			android.util.Log.d(tag, msg,tr);
+		}
+	}
+	
+	public static void v(final String tag,final String msg){
+		if (mDebug) {
+			android.util.Log.v(tag, msg);
+		}
+	}
+	
+	public static void v(final String tag,final String msg,final Throwable tr){
+		if (mDebug) {
+			android.util.Log.v(tag, msg,tr);
+		}
+	}
+	
+	public static void i(final String tag,final String msg){
+		if (mDebug) {
+			android.util.Log.i(tag, msg);
+		}
+	}
+	
+	public static void i(final String tag,final String msg,final Throwable tr){
+		if (mDebug) {
+			android.util.Log.i(tag, msg,tr);
+		}
+	}
+}
